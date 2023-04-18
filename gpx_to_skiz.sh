61abe12d86ead5ddb40b393e9c5fba8bab8fe93f
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2023-04-18 08:41:01 +0200

    Update gpx_to_skiz.sh

--- gpx_to_skiz.sh
+++ gpx_to_skiz.sh
@@ -125,8 +125,8 @@ for input in *.gpx; do
   # Create Track.xml - UID
   sed -i "s=VAR_parseObjectId=$(echo $RANDOM | md5sum | head -c 20; echo;)=g" gpx_to_skiz/"$filename"/Track.xml
   # Create Track.xml - Times
-  START_TIME="$(cat gpx_to_skiz/"$filename"/Nodes.csv | awk -F "," '{ print $1 }' | head -1 | sed 's/^/@/' | xargs date +"%Y-%m-%dT%H:%M:%S%:z" -d)"
-  END_TIME="$(cat gpx_to_skiz/"$filename"/Nodes.csv | awk -F "," '{ print $1 }' | tail -1 | sed 's/^/@/' | xargs date +"%Y-%m-%dT%H:%M:%S%:z" -d)"
+  START_TIME="$(cat gpx_to_skiz/"$filename"/Nodes.csv | awk -F "," '{ print $1 }' | head -1 | sed 's/^/@/' | xargs date +"%Y-%m-%dT%H:%M:%S%z" -d)"
+  END_TIME="$(cat gpx_to_skiz/"$filename"/Nodes.csv | awk -F "," '{ print $1 }' | tail -1 | sed 's/^/@/' | xargs date +"%Y-%m-%dT%H:%M:%S%z" -d)"
   sed -i "s=VAR_start=$START_TIME=g" gpx_to_skiz/"$filename"/Track.xml
   sed -i "s=VAR_finish=$END_TIME=g" gpx_to_skiz/"$filename"/Track.xml
   sed -i "s=VAR_duration=$(($(date -d ${END_TIME%.*} +%s)-$(date -d ${START_TIME%.*} +%s))).000=g" gpx_to_skiz/"$filename"/Track.xml
